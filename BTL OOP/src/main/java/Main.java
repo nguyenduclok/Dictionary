ca87f9d215cfc5f009740b927cd1c91323bdeb6e
nguyenduclok <89176269+nguyenduclok@users.noreply.github.com>
2022-04-19 12:30:01 +0700

    Update Main.java

--- BTL OOP/src/main/java/Main.java
+++ BTL OOP/src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         DictionaryCommandLine dictionaryCommandLine = new DictionaryCommandLine();
-        DictionaryCommandLineTest dictionaryCommandline = new DictionaryCommandLineTest();
+        DictionaryCommandline dictionaryCommandline = new DictionaryCommandline();
         dictionaryCommandLine.dictionaryBasic();
         dictionaryCommandline.showAllWords();
         try {
