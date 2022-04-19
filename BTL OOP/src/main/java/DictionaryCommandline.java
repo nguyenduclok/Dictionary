21606767121dd525a33d51cef53ec2c36d8eb8ef
nguyenduclok <89176269+nguyenduclok@users.noreply.github.com>
2022-04-19 14:35:51 +0700

    Delete DictionaryCommandline.java

--- BTL OOP/src/main/java/DictionaryCommandline.java
+++
@@ -1,8 +0,0 @@
-public class DictionaryCommandline extends Dictionary {
-    public void showAllWords() {
-        System.out.println("No\t| English\t|Vietnamese");
-        for (int i = 0; i < wordInDictionary.size(); i++) {
-            System.out.println((i + 1) + "\t" + wordInDictionary.get(i).toString());
-        }
-    }
-}
