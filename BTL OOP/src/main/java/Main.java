221f0bac54939ddbe3641dd5280eb4db274011ff
nguyenduclok <89176269+nguyenduclok@users.noreply.github.com>
2022-04-19 14:40:26 +0700

    Update Main.java

--- BTL OOP/src/main/java/Main.java
+++ BTL OOP/src/main/java/Main.java
@@ -2,12 +2,12 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        DictionaryCommandLine dictionaryCommandLine = new DictionaryCommandLine();
-        DictionaryCommandline dictionaryCommandline = new DictionaryCommandline();
-        dictionaryCommandLine.dictionaryBasic();
-        dictionaryCommandline.showAllWords();
+        DictionaryCommandLine dictonary = new DictionaryCommandLine();
+        DictionaryManagement newDictionary = new DictionaryManagement();
+        newDictionary.insertFromFile();
+        dictonary.showAllWords();
         try {
-            DictionaryManagement.dictionaryExportToFile();
+            newDictionary.dictionaryExportToFile();
         } catch (IOException e) {
             e.printStackTrace();
         }
